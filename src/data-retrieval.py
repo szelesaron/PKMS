2a7fd558678a172657ea93d37e68c67877fca6c6
szelesaron <szelesaron39@gmail.com>
2024-03-06 20:04:07 +0100

    removed again and cleaned up

--- src/data-retrieval.py
+++ src/data-retrieval.py
@@ -14,35 +14,41 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
-def main():
-    """Fetches the content of the last 3 received emails."""
+
+def authenticate(token_path: str = "src/data/token.json"):
+    """Authenticate the user and return the credentials."""
     creds = None
     # Load credentials from token.json if it exists
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
+                "src/data/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(token_path, "w") as token:
             token.write(creds.to_json())
+    return creds
 
+def get_emails(n : int, creds : Credentials):
+    """
+    Gets the last n emails from the user's inbox.
+    """
     try:
         # Call the Gmail API to fetch the last 3 messages
         service = build("gmail", "v1", credentials=creds)
-        messages = service.users().messages().list(userId="me", labelIds=["INBOX"], maxResults=3).execute().get("messages", [])
+        messages = service.users().messages().list(userId="me", labelIds=["INBOX"], maxResults=n).execute().get("messages", [])
 
         if not messages:
             print("No messages found.")
             return
 
-        print("Last 3 emails:")
+        print(f"Last {n} emails:")
         for message in messages:
             msg = service.users().messages().get(userId="me", id=message["id"]).execute()
             message_data = msg["payload"]["headers"]
@@ -61,5 +67,7 @@ def main():
         # Handle errors from Gmail API
         print(f"An error occurred: {error}")
 
+
 if __name__ == "__main__":
-    main()
+    credentials = authenticate()
+    get_emails(3, credentials)
