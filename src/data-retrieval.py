8e3dff7e27281e4aefa4b5d9d3dd90fcba9a1bfa
szelesaron <szelesaron39@gmail.com>
2024-04-03 21:32:25 +0200

    keep is tricky, drive should be fine, TODO: cleanupp

--- src/data-retrieval.py
+++ src/data-retrieval.py
@@ -13,13 +13,15 @@ from googleapiclient.errors import HttpError
 import base64  
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.metadata.readonly"]
+
 
 
 def authenticate(token_path: str = "src/data/token.json"):
     """Authenticate the user and return the credentials."""
     creds = None
     # Load credentials from token.json if it exists
+    # TODO: this expires and the refresh doesnt work?
     if os.path.exists(token_path):
         creds = Credentials.from_authorized_user_file(token_path, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -37,7 +39,7 @@ def authenticate(token_path: str = "src/data/token.json"):
     return creds
 
 
-  
+# TODO: improve parsing, handle email chains
 def get_emails(n : int, creds : Credentials):  
     """  
     Gets the last n emails from the user's inbox.  
@@ -77,17 +79,63 @@ def get_emails(n : int, creds : Credentials):
             body = base64.b64decode(data).decode('utf-8')
 
             # Printing the subject, sender's email and message 
-            print("--------------------------------")
+            print("######################################")
             print("Subject: ", subject) 
             print("From: ", sender) 
             print("Message: ", body) 
-            print("--------------------------------")
+            print("######################################")
             print('\n') 
         except Exception as e: 
             print("Unable to get the message: ", e)
             print(payload)
 
+# TODO: only for enterprise accounts
+def get_notes(creds : Credentials):
+    service = build('keep', 'v1', credentials=creds)
+    # Call the API
+    results = service.notes().list().execute()
+    print(results)
+
+
+
+def search_file(creds : Credentials):
+    """Search file in drive location"""
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("src/data/token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "src/data/credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+        with open("src/data/token.json", "w") as token:
+            token.write(creds.to_json())
+    service = build("drive", "v3", credentials=creds)
+
+    # Call the Drive v3 API
+    results = (
+        service.files()
+        .list(pageSize=10, fields="nextPageToken, files(id, name)")
+        .execute()
+    )
+    items = results.get("files", [])
+    return items
+
+
+
 
 if __name__ == "__main__":
     credentials = authenticate()
-    print(get_emails(6, credentials))
+
+    # print(get_emails(6, credentials))
+    # print(get_notes(credentials))
+    print(search_file(credentials))
+    
