69fa1c19bad8e00ff915ef1b5363e5e8f5e001cd
szelesaron <szelesaron39@gmail.com>
2024-04-04 21:02:27 +0200

    added calendar

--- src/data-retrieval.py
+++ src/data-retrieval.py
@@ -9,14 +9,14 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 import base64  
+from datetime import datetime
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.metadata.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/drive.readonly", "https://www.googleapis.com/auth/calendar.readonly"]
 
 
-def authenticate(token_path: str = "src/data/token.json"):
+def authenticate(token_path: str = "src/auth_data/token.json"):
     """Authenticate the user and return the credentials.
         If a new service is added, token.json needs to be deleted.
     """
@@ -31,7 +31,7 @@ def authenticate(token_path: str = "src/data/token.json"):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "src/data/credentials.json", SCOPES
+                "src/auth_data/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
@@ -99,26 +99,9 @@ def get_notes(creds : Credentials):
 
 
 # TODO: clean up auth
-def search_file(creds : Credentials):
+def get_drive_files(creds : Credentials):
     """Search file in drive location"""
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("src/data/token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "src/data/credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-        with open("src/data/token.json", "w") as token:
-            token.write(creds.to_json())
+
     service = build("drive", "v3", credentials=creds)
 
     # Call the Drive v3 API
@@ -132,11 +115,32 @@ def search_file(creds : Credentials):
 
 
 
+def get_calendar_events(creds : Credentials):
+    """Get the user's calendar events"""
+
+    service = build("calendar", "v3", credentials=creds)
+
+    # Call the Calendar v3 API
+    now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
+    return events
 
 if __name__ == "__main__":
     credentials = authenticate()
 
     # print(get_emails(6, credentials))
     # print(get_notes(credentials))
-    print(search_file(credentials))
+    # print(get_drive_files(credentials))
+    print(get_calendar_events(credentials))
     
