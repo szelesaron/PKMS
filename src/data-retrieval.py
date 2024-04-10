9000c952b241160a6d0d90a6cb250b03a994789b
szelesaron <szelesaron39@gmail.com>
2024-04-10 14:45:20 +0200

    calendar data ready

--- src/data-retrieval.py
+++ src/data-retrieval.py
@@ -80,11 +80,10 @@ def get_emails(n : int, creds : Credentials):
             body = base64.b64decode(data).decode('utf-8')
 
             # Printing the subject, sender's email and message 
-            print("######################################")
+            print("####################################################################")
             print("Subject: ", subject) 
             print("From: ", sender) 
             print("Message: ", body) 
-            print("######################################")
             print('\n') 
         except Exception as e: 
             print("Unable to get the message: ", e)
@@ -98,7 +97,6 @@ def get_notes(creds : Credentials):
     print(results)
 
 
-# TODO: clean up auth
 def get_drive_files(creds : Credentials):
     """Search file in drive location"""
 
@@ -113,27 +111,24 @@ def get_drive_files(creds : Credentials):
     items = results.get("files", [])
     return items
 
-from time import strptime
 
-def get_calendar_events(creds : Credentials) -> list:
+def get_calendar_events(n : int, creds : Credentials) -> list:
     """Get the user's calendar events
     :param creds: The user's credentials
     :return: The user's calendar events:
     
     In  this standard format: {'kind': 'calendar#event', 'etag': '"3424160851032000"', 'id': '7cr9l0ggad4dm9h68vdijaro5k', 'status': 'confirmed', 'htmlLink': 'https://www.google.com/calendar/event?eid=N2NyOWwwZ2dhZDRkbTloNjh2ZGlqYXJvNWsgc3plbGVzYXJvbjM5QG0', 'created': '2024-04-02T17:53:45.000Z', 'updated': '2024-04-02T17:53:45.516Z', 'summary': 'Fogorvos', 'creator': {'email': 'szelesaron39@gmail.com', 'self': True}, 'organizer': {'email': 'szelesaron39@gmail.com', 'self': True}, 'start': {'dateTime': '2024-04-05T10:30:00+02:00', 'timeZone': 'Europe/Budapest'}, 'end': {'dateTime': '2024-04-05T11:30:00+02:00', 'timeZone': 'Europe/Budapest'}, 'iCalUID': '7cr9l0ggad4dm9h68vdijaro5k@google.com', 'sequence': 0, 'reminders': {'useDefault': True}, 'eventType': 'default'}    
     """
-
-
     service = build("calendar", "v3", credentials=creds)
     # Call the Calendar v3 API
-    now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    # now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
     events_result = (
         service.events()
         .list(
             calendarId="primary",
             # this sets the start date
             # timeMin=now,
-            # maxResults=n,
+            maxResults=n,
             # singleEvents=True,
             # orderBy="startTime",
         )
@@ -165,14 +160,13 @@ def get_calendar_events(creds : Credentials) -> list:
                 }
             )
         except KeyError:
-            print("KeyError in getting event:")
-            print(event)
+            print("KeyError in getting event with link:", event["htmlLink"])
     return events_list
 
 if __name__ == "__main__":
     credentials = authenticate()
 
-    # print(get_emails(6, credentials))
+    print(get_emails(6, credentials))
     # print(get_notes(credentials))
     # print(get_drive_files(credentials))
-    print(get_calendar_events(credentials))
+    # print(get_calendar_events(n=1000, creds=credentials))
